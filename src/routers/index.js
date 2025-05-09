8f2ce06a5b8d163f4c24d610f8f819bc790c9773
xAarT <arturbresolin20@gmail.com>
2025-05-08 21:06:43 -0300

    ajustado irregularidades no merge

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,22 +2,12 @@ const consultas = require('./consultasRouter');
 const pacientes = require('./pacientesRouter');
 const medicos = require('./medicosRouter');
 const usuarios = require('./usuariosRouter');
-<<<<<<< HEAD
 const auth = require('./authRoutes');
-=======
-const pagamentos = require('./pagamentosRouter');
-const itens = require('./itensRouter');
->>>>>>> e692e45fc6cb8925ae4775487c214fd137294794
 
 module.exports = (app) => {
   consultas(app);
   pacientes(app);
   medicos(app);
   usuarios(app);
-<<<<<<< HEAD
   auth(app);
-=======
-  pagamentos(app);
-  itens(app);
->>>>>>> e692e45fc6cb8925ae4775487c214fd137294794
 };
